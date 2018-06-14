3dca8b9a1a9bbdf5804871f391791343dcced5c7
Hyeon Uk Jeong <hyeonukjeong@Hyeons-MacBook-Pro.local>
2018-06-14 15:22:58 +0900

    fixed segue

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Hyeon Uk Jeong on 6/14/18.
+//  Copyright © 2018 Hyeon Uk Jeong. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+    
+    @IBAction func onNextPressed(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
